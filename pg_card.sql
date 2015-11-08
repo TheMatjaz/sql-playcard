bc3a567787db27b90e54ab56fd63dffe5073a89a
Matjaž <dev@matjaz.it>
2015-11-08 12:39:42 +0100

    Add function to get playcard value as int
    "playcard_value_as_int" returns an int indicating the value of the card
    for cards from ace to king, with values from 1 to 13. Returns NULL as
    value of the joker and the card back.

--- pg_card.sql
+++ pg_card.sql
@@ -17,4 +17,28 @@ DROP TYPE IF EXISTS playcard_suit_enum;
 CREATE TYPE playcard_suit_enum
     AS ENUM ('hearts', 'diamonds', 'clubs', 'spades');
 
+CREATE OR REPLACE FUNCTION playcard_value_as_int(card_id playcard)
+    RETURNS int
+    LANGUAGE sql
+    AS $body$
+        SELECT
+            CASE substring(card_id for 1)
+                WHEN 'A' THEN 1
+                WHEN '2' THEN 2
+                WHEN '3' THEN 3
+                WHEN '4' THEN 4
+                WHEN '5' THEN 5
+                WHEN '6' THEN 6
+                WHEN '7' THEN 7
+                WHEN '8' THEN 8
+                WHEN '9' THEN 9
+                WHEN '0' THEN 10
+                WHEN 'J' THEN 11
+                WHEN 'Q' THEN 12
+                WHEN 'K' THEN 13
+                ELSE NULL -- for joker 'S_' and card back 'B_'
+            END;
+    $body$;
+
+
 COMMIT;
