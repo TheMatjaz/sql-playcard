f490d58d4d3909516dccebe782f8c3661f5c9641
Matjaž <dev@matjaz.it>
2016-04-05 15:30:24 +0200

    Add value symbol and use char/varchar instead of text
    The value symbol is a char(2) that shows the value of a card in a
    compact way. It's a string alternative to the value_smallint. The
    difference is visible for:
    
    - ace: 'A' instead of 1
    - jack: 'J' instead of 11
    - queen: 'Q' instead of 12
    - king: 'K' instead of 13
    - joker: '★' or '☆' instead of NULL since it has no value
    
    This could be useful when rendering the actual card on screen: an ace of
    spades becomes just 'A♠' which is value_symbol || suit_symbol.
    
    *****
    
    The usage of char and varchar instead of text is for SQL code
    portability and to enforce the string length constraint. For PostgreSQL
    char/varchar/text makes no difference under the hood, but other RDBMS
    may operate differently.
    
    So for now to make code portable, just remove the CONSTRAINT clauses,
    convert enums to varchar(8) and remove the function is_empty().

--- pg_playcard_table.sql
+++ pg_playcard_table.sql
@@ -21,11 +21,12 @@ DROP TABLE IF EXISTS playcards;
 
 CREATE TABLE playcards (
     id             smallint
-  , value_smallint smallint            -- NULL when Joker or covered
-  , value_text     text                -- NULL when Joker or covered
-  , suit_symbol    char(1)             -- NULL when Joker or covered
-  , suit_text      playcard_enum_suit  -- NULL when Joker or covered
-  , suit_color     playcard_enum_color -- NULL when Joker or covered
+  , value_smallint smallint            -- NULL when Joker or covered card
+  , value_text     varchar(5)          -- NULL when Joker or covered card
+  , value_symbol   char(2)             -- NULL when Joker or covered card
+  , suit_symbol    char(1)             -- NULL when Joker or covered card
+  , suit_text      playcard_enum_suit  -- NULL when Joker or covered card
+  , suit_color     playcard_enum_color -- NULL when Joker or covered card
   , unicode_char   char(1)             NOT NULL UNIQUE
 
   , PRIMARY KEY (id)
@@ -35,6 +36,8 @@ CREATE TABLE playcards (
         CHECK (value_smallint > 0 AND value_smallint <= 13)
   , CONSTRAINT value_text_not_empty
         CHECK (NOT is_empty(value_text))
+  , CONSTRAINT value_symbol_not_empty
+        CHECK (NOT is_empty(value_symbol))
   , CONSTRAINT suit_symbol_not_empty
         CHECK (NOT is_empty(suit_symbol))
   , CONSTRAINT uicode_char_not_empty
@@ -42,75 +45,75 @@ CREATE TABLE playcards (
     );
 
 INSERT INTO playcards 
-(id, unicode_char, value_smallint, value_text, suit_symbol, suit_text, suit_color)
+(id, unicode_char, value_smallint, value_text, value_symbol, suit_symbol, suit_text, suit_color)
 VALUES
 
     -- HEARTS
-    ( 1, '🂱',  1, 'ace',   '♥', 'hearts',   'red')
-  , ( 2, '🂲',  2, 'two',   '♥', 'hearts',   'red')
-  , ( 3, '🂳',  3, 'three', '♥', 'hearts',   'red')
-  , ( 4, '🂴',  4, 'four',  '♥', 'hearts',   'red')
-  , ( 5, '🂵',  5, 'five',  '♥', 'hearts',   'red')
-  , ( 6, '🂶',  6, 'six',   '♥', 'hearts',   'red')
-  , ( 7, '🂷',  7, 'seven', '♥', 'hearts',   'red')
-  , ( 8, '🂸',  8, 'eight', '♥', 'hearts',   'red')
-  , ( 9, '🂹',  9, 'nine',  '♥', 'hearts',   'red')
-  , (10, '🂺', 10, 'ten',   '♥', 'hearts',   'red')
-  , (11, '🂻', 11, 'jack',  '♥', 'hearts',   'red')
-  , (12, '🂽', 12, 'queen', '♥', 'hearts',   'red')
-  , (13, '🂾', 13, 'king',  '♥', 'hearts',   'red')
+    ( 1, '🂱',  1, 'ace',   'A', '♥', 'hearts',   'red')
+  , ( 2, '🂲',  2, 'two',   '2', '♥', 'hearts',   'red')
+  , ( 3, '🂳',  3, 'three', '3', '♥', 'hearts',   'red')
+  , ( 4, '🂴',  4, 'four',  '4', '♥', 'hearts',   'red')
+  , ( 5, '🂵',  5, 'five',  '5', '♥', 'hearts',   'red')
+  , ( 6, '🂶',  6, 'six',   '6', '♥', 'hearts',   'red')
+  , ( 7, '🂷',  7, 'seven', '7', '♥', 'hearts',   'red')
+  , ( 8, '🂸',  8, 'eight', '8', '♥', 'hearts',   'red')
+  , ( 9, '🂹',  9, 'nine',  '9', '♥', 'hearts',   'red')
+  , (10, '🂺', 10, 'ten',  '10', '♥', 'hearts',   'red')
+  , (11, '🂻', 11, 'jack',  'J', '♥', 'hearts',   'red')
+  , (12, '🂽', 12, 'queen', 'Q', '♥', 'hearts',   'red')
+  , (13, '🂾', 13, 'king',  'K', '♥', 'hearts',   'red')
 
     -- DIAMONDS
-  , (14, '🃁',  1, 'ace',   '♦', 'diamonds', 'red')
-  , (15, '🃂',  2, 'two',   '♦', 'diamonds', 'red')
-  , (16, '🃃',  3, 'three', '♦', 'diamonds', 'red')
-  , (17, '🃄',  4, 'four',  '♦', 'diamonds', 'red')
-  , (18, '🃅',  5, 'five',  '♦', 'diamonds', 'red')
-  , (19, '🃆',  6, 'six',   '♦', 'diamonds', 'red')
-  , (20, '🃇',  7, 'seven', '♦', 'diamonds', 'red')
-  , (21, '🃈',  8, 'eight', '♦', 'diamonds', 'red')
-  , (22, '🃉',  9, 'nine',  '♦', 'diamonds', 'red')
-  , (23, '🃊', 10, 'ten',   '♦', 'diamonds', 'red')
-  , (24, '🃋', 11, 'jack',  '♦', 'diamonds', 'red')
-  , (25, '🃍', 12, 'queen', '♦', 'diamonds', 'red')
-  , (26, '🃎', 13, 'king',  '♦', 'diamonds', 'red')
+  , (14, '🃁',  1, 'ace',   'A', '♦', 'diamonds', 'red')
+  , (15, '🃂',  2, 'two',   '2', '♦', 'diamonds', 'red')
+  , (16, '🃃',  3, 'three', '3', '♦', 'diamonds', 'red')
+  , (17, '🃄',  4, 'four',  '4', '♦', 'diamonds', 'red')
+  , (18, '🃅',  5, 'five',  '5', '♦', 'diamonds', 'red')
+  , (19, '🃆',  6, 'six',   '6', '♦', 'diamonds', 'red')
+  , (20, '🃇',  7, 'seven', '7', '♦', 'diamonds', 'red')
+  , (21, '🃈',  8, 'eight', '8', '♦', 'diamonds', 'red')
+  , (22, '🃉',  9, 'nine',  '9', '♦', 'diamonds', 'red')
+  , (23, '🃊', 10, 'ten',  '10', '♦', 'diamonds', 'red')
+  , (24, '🃋', 11, 'jack',  'J', '♦', 'diamonds', 'red')
+  , (25, '🃍', 12, 'queen', 'Q', '♦', 'diamonds', 'red')
+  , (26, '🃎', 13, 'king',  'K', '♦', 'diamonds', 'red')
 
     -- CLUBS
-  , (27, '🃑',  1, 'ace',   '♣', 'clubs',    'black')
-  , (28, '🃒',  2, 'two',   '♣', 'clubs',    'black')
-  , (29, '🃓',  3, 'three', '♣', 'clubs',    'black')
-  , (30, '🃔',  4, 'four',  '♣', 'clubs',    'black')
-  , (31, '🃕',  5, 'five',  '♣', 'clubs',    'black')
-  , (32, '🃖',  6, 'six',   '♣', 'clubs',    'black')
-  , (33, '🃗',  7, 'seven', '♣', 'clubs',    'black')
-  , (34, '🃘',  8, 'eight', '♣', 'clubs',    'black')
-  , (35, '🃙',  9, 'nine',  '♣', 'clubs',    'black')
-  , (36, '🃚', 10, 'ten',   '♣', 'clubs',    'black')
-  , (37, '🃛', 11, 'jack',  '♣', 'clubs',    'black')
-  , (38, '🃝', 12, 'queen', '♣', 'clubs',    'black')
-  , (39, '🃞', 13, 'king',  '♣', 'clubs',    'black')
+  , (27, '🃑',  1, 'ace',   'A', '♣', 'clubs',    'black')
+  , (28, '🃒',  2, 'two',   '2', '♣', 'clubs',    'black')
+  , (29, '🃓',  3, 'three', '3', '♣', 'clubs',    'black')
+  , (30, '🃔',  4, 'four',  '4', '♣', 'clubs',    'black')
+  , (31, '🃕',  5, 'five',  '5', '♣', 'clubs',    'black')
+  , (32, '🃖',  6, 'six',   '6', '♣', 'clubs',    'black')
+  , (33, '🃗',  7, 'seven', '7', '♣', 'clubs',    'black')
+  , (34, '🃘',  8, 'eight', '8', '♣', 'clubs',    'black')
+  , (35, '🃙',  9, 'nine',  '9', '♣', 'clubs',    'black')
+  , (36, '🃚', 10, 'ten',  '10', '♣', 'clubs',    'black')
+  , (37, '🃛', 11, 'jack',  'J', '♣', 'clubs',    'black')
+  , (38, '🃝', 12, 'queen', 'Q', '♣', 'clubs',    'black')
+  , (39, '🃞', 13, 'king',  'K', '♣', 'clubs',    'black')
 
     -- SPADES
-  , (40, '🂡',  1, 'ace',   '♠', 'spades',   'black')
-  , (41, '🂢',  2, 'two',   '♠', 'spades',   'black')
-  , (42, '🂣',  3, 'three', '♠', 'spades',   'black')
-  , (43, '🂤',  4, 'four',  '♠', 'spades',   'black')
-  , (44, '🂥',  5, 'five',  '♠', 'spades',   'black')
-  , (45, '🂦',  6, 'six',   '♠', 'spades',   'black')
-  , (46, '🂧',  7, 'seven', '♠', 'spades',   'black')
-  , (47, '🂨',  8, 'eight', '♠', 'spades',   'black')
-  , (48, '🂩',  9, 'nine',  '♠', 'spades',   'black')
-  , (49, '🂪', 10, 'ten',   '♠', 'spades',   'black')
-  , (50, '🂫', 11, 'jack',  '♠', 'spades',   'black')
-  , (51, '🂭', 12, 'queen', '♠', 'spades',   'black')
-  , (52, '🂮', 13, 'king',  '♠', 'spades',   'black')
+  , (40, '🂡',  1, 'ace',   'A', '♠', 'spades',   'black')
+  , (41, '🂢',  2, 'two',   '2', '♠', 'spades',   'black')
+  , (42, '🂣',  3, 'three', '3', '♠', 'spades',   'black')
+  , (43, '🂤',  4, 'four',  '4', '♠', 'spades',   'black')
+  , (44, '🂥',  5, 'five',  '5', '♠', 'spades',   'black')
+  , (45, '🂦',  6, 'six',   '6', '♠', 'spades',   'black')
+  , (46, '🂧',  7, 'seven', '7', '♠', 'spades',   'black')
+  , (47, '🂨',  8, 'eight', '8', '♠', 'spades',   'black')
+  , (48, '🂩',  9, 'nine',  '9', '♠', 'spades',   'black')
+  , (49, '🂪', 10, 'ten',  '10', '♠', 'spades',   'black')
+  , (50, '🂫', 11, 'jack',  'J', '♠', 'spades',   'black')
+  , (51, '🂭', 12, 'queen', 'Q', '♠', 'spades',   'black')
+  , (52, '🂮', 13, 'king',  'K', '♠', 'spades',   'black')
 
     -- JOKERS
-  , (53, '🃟', NULL, 'joker', NULL, NULL, 'red')
-  , (54, '🃏', NULL, 'joker', NULL, NULL, 'black')
+  , (53, '🃟', NULL, 'joker', '☆', NULL, NULL, 'red')
+  , (54, '🃏', NULL, 'joker', '★', NULL, NULL, 'black')
 
     -- COVERED CARD / BACK OF A CARD
-  , ( 0, '🂠', NULL, NULL, NULL, NULL, NULL)
+  , ( 0, '🂠', NULL, NULL, NULL, NULL, NULL, NULL)
   ;
 
 COMMIT;
